724f5be5b4f95d861452c28fc1681d30a4c5607e
김태주 <kim9xd@gmail.com>
2021-04-01 23:09:49 +0900

    요청 맵핑하기 4부 헤더와 매개변수 누락 부분 추가

--- src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +24,11 @@ public class SampleControllerTest {
 	public void helloTest() throws Exception {
 		
 		mockMvc.perform(get("/hello")
+				// header중 From의 값을 localhost로 보내준다.
+//					.header(HttpHeaders.FROM, "localhost" ))
+				// header의 Authorizationn값을 111로 보내준다.
+//					.header(HttpHeaders.AUTHORIZATION, "111"))
+//				 parameter의 name을 name, value를 taeju로 보내준다.
 					.param("name", "taeju"))
 				.andDo(print())
 				.andExpect(status().isOk());
