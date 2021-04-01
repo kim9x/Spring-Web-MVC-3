9141fc3047361656ca0e2684bbdb81b4460d052c
김태주 <kim9xd@gmail.com>
2021-04-02 01:03:27 +0900

    요청 매핑하기 5부 HEAD와 OPTIONS

--- src/main/java/me/pulpury/demowebmvc/SampleController.java
+++ src/main/java/me/pulpury/demowebmvc/SampleController.java
@@ -1,25 +1,30 @@
 package me.pulpury.demowebmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-// 만약 Class와 Method 모두 consumes와 produces 사용 시
-// method에서 overinding해서 덮어쓰기 해버림.
 public class SampleController {
-	
-	// Header가 불일치하는 경우 
-//	@RequestMapping(value = "/hello", headers = "!"+HttpHeaders.FROM)
-	// Header가 일치하는 경우
-//	@RequestMapping(value = "/hello", headers = HttpHeaders.AUTHORIZATION + "=" + "111")
-	// Name이라는 Parameter가 있어야하는 경우
-//	@RequestMapping(value = "/hello", params = "taeju")
-	// Parameter가 일치하는 경우
-	@RequestMapping(value = "/hello", params = "name=taeju")
+	 
+	@GetMapping("/hello")
 	@ResponseBody
-	public String hello() {
+	public String helloGet() {
 		return "hello";
 	}
+	
+	@PostMapping("/hello")
+	@ResponseBody
+	public String helloPost() {
+		return "hello";
+	}
+	
+//	@RequestMapping("/hello")
+//	@ResponseBody
+//	public String hello() {
+//		return "hello";
+//	}
 
 }
--- src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
@@ -1,7 +1,10 @@
 package me.pulpury.demowebmvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,23 +25,30 @@ public class SampleControllerTest {
 	@Test
 	public void helloTest() throws Exception {
 		
-		mockMvc.perform(get("/hello")
-				// header중 From의 값을 localhost로 보내준다.
-//					.header(HttpHeaders.FROM, "localhost" ))
-				// header의 Authorizationn값을 111로 보내준다.
-//					.header(HttpHeaders.AUTHORIZATION, "111"))
-//				 parameter의 name을 name, value를 taeju로 보내준다.
-					.param("name", "taeju"))
+		// HEAD요청은 GET요청과 동일하지만 응답 본문을 받아오지 않고 응답 헤더만 받아온다.
+		// response에 body를 실어보내지 않는다.
+		// GET요청으로 정보를 가져오기 전에
+		// header정보로 resource의 정보를 확인만 하는 것이지 떄문
+//		mockMvc.perform(head("/hello"))
+		
+		// options는 사용할 수 있는 HTTP Method 제공
+		// 서버 또는 특정 리소스가 제공하는 기능 확인 가능
+		// 서버에서 Allow 응답 헤더에 사용할 수 있는 HTTP Method 목록을 제공
+		mockMvc.perform(options("/hello"))
 				.andDo(print())
-				.andExpect(status().isOk());
-		
-//		mockMvc.perform(get("/hi"))
-//		.andDo(print())
-//		.andExpect(status().isOk());
-		
-//		mockMvc.perform(put("/hello"))
-//		.andDo(print())
-//		.andExpect(status().isMethodNotAllowed());
+				.andExpect(status().isOk())
+				.andExpect(header().exists(HttpHeaders.ALLOW))
+				// 밑의 코드는 stringValues 때문에 순서까지 맞춰줘야한다.
+				// 
+//				.andExpect(header().stringValues(HttpHeaders.ALLOW, "GET", "POST", "OPTIONS", "HEAD"));
+				
+				//아래 코드는 순서가 상관이 없지만... 좀 번거롭..
+				.andExpect(header().stringValues(HttpHeaders.ALLOW
+						, hasItems(
+									containsString("GET")
+									, containsString("POST")
+									, containsString("HEAD")
+									, containsString("OPTIONS"))));
 	}
 
 }
