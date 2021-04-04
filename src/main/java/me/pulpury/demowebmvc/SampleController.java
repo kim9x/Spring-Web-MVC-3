4ff8b47e61fed77938ac5e87fe868c686567a4f8
김태주 <kim9xd@gmail.com>
2021-04-04 17:46:55 +0900

    6부 커스텀 애노테이션  - Meta Annotationn (Annotationn 위에 사용 가능 한 것)  - Composed Annotation (@GetMapping같이 여러 Annotation으로 이루어 진 것)
     @Retention을 사용하여 해당 Annotation 정보를 언제까지 유지할 것인가 설정할 수 있다.
     RetentionPolicy.RUNTIME을 줘야 RUNTIME 시에 사용할 수 있다.
     RetentionPolicy.CLASS는 .class에는 남아 있지만.. RUNTIM 시엔 사라진다.
     RetentionPolicy.SOURCE는 소스코드 내에서만 볼 수 있으며.. compile하고 나서는 없어진다.
     => 주석처럼 사용하고 싶을 때 사용 가능.
    
     @Target을 사용하여 어디서 사용할 수 있는지 명시한다.
     @Documented는 문서화가 가능하다(?)

--- src/main/java/me/pulpury/demowebmvc/SampleController.java
+++ src/main/java/me/pulpury/demowebmvc/SampleController.java
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 	 
-	@GetMapping("/hello")
+	// @GetMapping처럼 Annotation이 모여서 만들어 진 것 Composed Annotation이라고 부를 수 있다.
+	// @GetMapping을 들여다보면 @RequestMapping 등등이 있는데 이렇게
+	// Annotationn 위에 사용하는 Annotation을 Meta Annotation이라고 부릅니다.
+//	@GetMapping("/hello")
+	// Custom하여 사용할 수 있다.
+	@GetHelloMapping
 	@ResponseBody
 	public String helloGet() {
-		return "hello";
-	}
-	
-	@PostMapping("/hello")
-	@ResponseBody
-	public String helloPost() {
 		return "hello";
 	}
 	
--- src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
@@ -2,6 +2,7 @@ package me.pulpury.demowebmvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -24,31 +25,9 @@ public class SampleControllerTest {
 	
 	@Test
 	public void helloTest() throws Exception {
-		
-		// HEAD요청은 GET요청과 동일하지만 응답 본문을 받아오지 않고 응답 헤더만 받아온다.
-		// response에 body를 실어보내지 않는다.
-		// GET요청으로 정보를 가져오기 전에
-		// header정보로 resource의 정보를 확인만 하는 것이지 떄문
-//		mockMvc.perform(head("/hello"))
-		
-		// options는 사용할 수 있는 HTTP Method 제공
-		// 서버 또는 특정 리소스가 제공하는 기능 확인 가능
-		// 서버에서 Allow 응답 헤더에 사용할 수 있는 HTTP Method 목록을 제공
-		mockMvc.perform(options("/hello"))
+		mockMvc.perform(get("/hello"))
 				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(header().exists(HttpHeaders.ALLOW))
-				// 밑의 코드는 stringValues 때문에 순서까지 맞춰줘야한다.
-				// 
-//				.andExpect(header().stringValues(HttpHeaders.ALLOW, "GET", "POST", "OPTIONS", "HEAD"));
-				
-				//아래 코드는 순서가 상관이 없지만... 좀 번거롭..
-				.andExpect(header().stringValues(HttpHeaders.ALLOW
-						, hasItems(
-									containsString("GET")
-									, containsString("POST")
-									, containsString("HEAD")
-									, containsString("OPTIONS"))));
+				.andExpect(status().isOk());
 	}
 
 }
