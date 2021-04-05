05be4731970969894c7e785e81798f5cd8f595f9
김태주 <kim9xd@gmail.com>
2021-04-06 00:33:05 +0900

    핸들러 메소드 3부 요청 매개변수 (단순 타입)  - @RequestParam

--- src/main/java/me/pulpury/demowebmvc/SampleController.java
+++ src/main/java/me/pulpury/demowebmvc/SampleController.java
@@ -1,41 +1,44 @@
 package me.pulpury.demowebmvc;
 
-import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class SampleController {
 	
-	@GetMapping("/events/{id}")
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	
+	@PostMapping("/events")
 	@ResponseBody
 	public Event getEvent(
+			// '@RequestParam'을 제외하고 String name이런 식으로 생략 또한 가능하다..?
+			// 그러나 추천하지는 않음. 팀원들이 모두 다 Spring MVC에 익숙하지 않을 수도 있기 때문에
+			// 명시적으로 선호해주는 것이 좋다.
+			// 스타 개발자가 아닌 협업을 잘하는 개발자가 되자.
+			@RequestParam String name
+			, @RequestParam Integer limit
 			
-			// {id}와 @PathVariable의 변수명(ex. 바로 밑의 Integer id)을 맞춰주면
-			// 따로 설정없이도 사용이 가능하다.
-			@PathVariable Integer id
-			// 아래처럼 {id} 값을 @PathVariable("id")로 명시해주면
-			// idValue같은 다른 변수명을 사용할 수 있다.
-//			, @PathVariable("id") Integer idValue
-			, @MatrixVariable String name) {
+			// @RequestParam 또한 변수명을 맞춰주고 코드를 줄이거나
+			// @ReauestParma(value = "name")을 사용해서 아예 다른 변수(ex. nameValue) 등을 
+			// 사용할 수도 있다.
+//			, @RequestParam(value = "name", required = false, defaultValue = "taeju") String nameValue
+			
+			// Map으로 전달받고 event.setName(params.get("name"));
+			// 이런식으로 알고 있는 변수명으로 사용할 수도 있다.
+//			, @RequestParam Map<String, String> params
+			) {
 		Event event = new Event();
-		event.setId(id);
-//		event.setId(idValue);
 		event.setName(name);
+		event.setLimit(limit);
+		logger.info("event", event);
+//		event.setName(params.get("name"));
 		return event;
 		
 	}
--- src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/pulpury/demowebmvc/SampleControllerTest.java
@@ -26,10 +26,16 @@ public class SampleControllerTest {
 	
 	@Test
 	public void getEvent() throws Exception {
-		mockMvc.perform(get("/events/1;name=taeju"))
+//		mockMvc.perform(post("/events?name=taeju"))
+		// 위에 것이나 아래 것이나 똑같다.
+		// 아래처럼 사용할거면 문자열만 줄 수 있다.
+		mockMvc.perform(post("/events")
+					.param("name", "taeju")
+					.param("limit", "20"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("id").value(1));
+//				.andExpect(jsonPath("id").value(1));
+				.andExpect(jsonPath("name").value("taeju"));
 				;
 	}
 
